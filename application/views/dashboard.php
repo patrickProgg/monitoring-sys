7709602ad44bf601bad6578da9aa6d27181d7f18
patrickProgg <patrickhenrybersaluna115@gmail.com>
2026-07-13 10:15:38 +0800

    t

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,7 +1,7 @@
 <style>
     .box-info {
         display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
         grid-gap: 24px;
         padding-left: 0;
         padding-right: 0;
