71cd024d784168881e58c699fe721e395132c00b
patrickProgg <patrickhenrybersaluna115@gmail.com>
2026-07-13 10:42:24 +0800

    t

--- application/controllers/View_ui_cont.php
+++ application/controllers/View_ui_cont.php
@@ -704,7 +704,7 @@ class View_ui_cont extends CI_Controller
             ->join('tbl_client as b', 'b.id = a.cl_id', 'left')
             // ->where('b.status !=', '1')
             ->where('YEAR(a.start_date)', $year)
-            ->where_in('a.status', ['ongoing', 'completed'])  // Add this line
+            ->where_in('a.status', ['ongoing', 'completed'])
             ->group_by('MONTH(a.start_date)')
             ->order_by('MONTH(a.start_date)');
 
@@ -724,9 +724,8 @@ class View_ui_cont extends CI_Controller
             ->from('tbl_loan as a')
             ->join('tbl_client as b', 'b.id = a.cl_id', 'left')
             // ->where('b.status !=', '1')
-            ->where('YEAR(a.start_date)', $year)
-            ->where_in('a.status', ['ongoing', 'completed'])  // Add this line
-            ->get();
+            ->where_in('a.status', ['ongoing', 'completed'])
+            ->where('YEAR(a.start_date)', $year);
         $year_total_query = $this->db->get();
         $year_total = $year_total_query->row()->capital_amt ?: 0;
 
